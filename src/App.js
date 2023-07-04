f6c6dac299faff468e03d000402311ffead73367
Marcia de Jesus Ferreira <marcia-j-ferreira@telecom.pt>
2023-07-04 15:45:08 +0100

    change app text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I was changed 
+          I was changed on the Feature branch 
         </a>
       </header>
     </div>
